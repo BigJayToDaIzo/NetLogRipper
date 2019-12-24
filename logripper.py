6184a03b37c020f375859109d259fcf4e53e8ada
Joseph Myers <josephlmyers@gmail.com>
2019-12-23 23:00:47 -0600

    Write time parsing tests as well as method tests

--- logripper.py
+++ logripper.py
@@ -6,22 +6,89 @@ from datetime import datetime
 
 class LogRipper(object):
     def __init__(self):
-        self.days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
         self.connected = True
         self.not_connected = False
         self.disconnections = 0
-        self.seconds_disconnected = 0
-        self.began_monitoring = ''
-        self.ended_monitoring = ''
-        self.file_name = ''
+        self.began_monitoring = None
+        self.ended_monitoring = None
+        self.file_name = None
         self.file_stream = None
         self.lines = []
-        self.words = None
+        self.total_seconds_logged = 0
         self.total_minutes_logged = 0
         self.total_hours_logged = 0
         self.total_days_logged = 0
+        self.total_weeks_logged = 0
         self.total_months_logged = 0
+        self.total_seconds_disconnected = 0
+        self.average_downtime = 0
+        self.average_uninterrupted_uptime = 0
+        self.packet_loss = 0
 
+
+    def __str__(self):
+        return "connected: {0}\n" \
+               "not_connected: {1}\n" \
+               "disconnections: {2}\n" \
+               "seconds_disconnected: {3}\n" \
+               "began_monitoring: {4}\n" \
+               "ended_monitoring: {5}\n" \
+               "file_name: {6}\n" \
+               "lines: <use print(log_ripper.get_lines_array())>\n" \
+               "total_seconds_logged: {7}\n" \
+               "total_minutes_logged: {8}\n" \
+               "total_hours_logged: {9}\n" \
+               "total_days_logged: {10}\n" \
+               "total_weeks_logged: {11}\n" \
+               "total_months_logged: {12}".format(
+                                                self.connected,
+                                                self.not_connected,
+                                                self.disconnections,
+                                                self.seconds_disconnected,
+                                                self.get_began_monitoring(),
+                                                self.get_ended_monitoring(),
+                                                self.file_name,
+                                                self.total_seconds_logged,
+                                                self.total_minutes_logged,
+                                                self.total_hours_logged,
+                                                self.total_days_logged,
+                                                self.total_weeks_logged,
+                                                self.total_months_logged
+                                            )
+
+    """ Class Getters """
+    def get_began_monitoring(self):
+        return "{0}/{1}/{2} {3}:{4}:{5}".format(
+            self.began_monitoring.month,
+            self.began_monitoring.day,
+            self.began_monitoring.year,
+            self.began_monitoring.hour,
+            self.began_monitoring.minute,
+            self.began_monitoring.second
+        )
+
+    def get_ended_monitoring(self):
+        return "{0}/{1}/{2} {3}:{4}:{5}".format(
+            self.ended_monitoring.month,
+            self.ended_monitoring.day,
+            self.ended_monitoring.year,
+            self.ended_monitoring.hour,
+            self.ended_monitoring.minute,
+            self.ended_monitoring.second
+        )
+
+    def get_lines_array(self):
+        """
+        Summary line.
+        Function returns a string representation of the classes lines array
+        Parameters:
+            None
+        Returns:
+            String: Class list array to string
+        """
+        return '\n'.join(self.lines)
+
+    """ Class Setters """
     def set_filename(self):
         if len(sys.argv) < 2:
             print("Usage: python logripper.py <logfile.txt>")
@@ -29,6 +96,25 @@ class LogRipper(object):
         else:
             self.file_name = sys.argv[1]
 
+    def set_began_monitoring(self):
+        timestamp = int(self.lines[1].split()[0].strip('[]').split('.')[0])
+        begin_date_and_time = datetime.fromtimestamp(timestamp)
+        self.began_monitoring = begin_date_and_time
+
+    def set_ended_monitoring(self):
+        timestamp = int(self.lines[len(self.lines) - 1].split()[0].strip('[]').split('.')[0])
+        end_date_and_time = datetime.fromtimestamp(timestamp)
+        self.ended_monitoring = end_date_and_time
+
+    def set_connected(self, connected):
+        self.connected = connected
+
+    def set_not_connected(self, not_connected):
+        self.not_connected = not_connected
+
+
+
+    """ Class Methods """
     def open_file_stream(self):
         try:
             self.file_stream = open(self.file_name, 'r')
@@ -39,7 +125,7 @@ class LogRipper(object):
 
     def populate_lines_array(self):
         for line in self.file_stream:
-            self.lines.append(line)
+            self.lines.append(line.strip('\n'))
 
     def populate_words_array(self):
         word_array_height = len(self.lines)
@@ -54,10 +140,21 @@ class LogRipper(object):
             self.words[i] = line.split()
             i += 1
 
-    def set_began_monitoring(self):
-        timestamp = int(self.lines[1].split()[0].strip('[]').split('.')[0])
-        begin_date_and_time = datetime.fromtimestamp(timestamp)
-        self.began_monitoring = begin_date_and_time
+    def calculate_totals_logged(self):
+        duration = self.ended_monitoring - self.began_monitoring
+        self.total_seconds_logged = duration.total_seconds()
+        self.total_minutes_logged = self.total_seconds_logged / 60
+        self.total_hours_logged = self.total_minutes_logged / 60
+        self.total_days_logged = self.total_hours_logged / 24
+        self.total_weeks_logged = self.total_days_logged / 7
+        self.total_months_logged = self.total_weeks_logged / 4
 
-    def set_ended_monitoring(self):
-        timestamp = self.lines[len(self.lines) - 1].split()
+    def check_line_for_connected(self, line):
+        if line.split()[1] != '64':
+            self.set_connected(False)
+            self.set_not_connected(True)
+            return False
+        else:
+            self.set_connected(True)
+            self.set_not_connected(False)
+            return True
--- test/test_log_ripper.py
+++ test/test_log_ripper.py
@@ -10,24 +10,25 @@ sys.path.append('../')
 class TestLogRipperInit(unittest.TestCase):
     def test_class_init(self):
         log_ripper = LogRipper()
-        self.assertEqual(log_ripper.days_of_week, ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
         self.assertEqual(log_ripper.connected, True)
         self.assertEqual(log_ripper.not_connected, False)
         self.assertEqual(log_ripper.disconnections, 0)
-        self.assertEqual(log_ripper.seconds_disconnected, 0)
-        self.assertEqual(log_ripper.began_monitoring, '')
-        self.assertEqual(log_ripper.ended_monitoring, '')
-        self.assertEqual(log_ripper.file_name, '')
+        self.assertEqual(log_ripper.began_monitoring, None)
+        self.assertEqual(log_ripper.ended_monitoring, None)
+        self.assertEqual(log_ripper.file_name, None)
         self.assertEqual(log_ripper.file_stream, None)
         self.assertEqual(log_ripper.lines, [])
-        self.assertEqual(log_ripper.words, None)
         self.assertEqual(log_ripper.total_minutes_logged, 0)
         self.assertEqual(log_ripper.total_hours_logged, 0)
         self.assertEqual(log_ripper.total_days_logged, 0)
         self.assertEqual(log_ripper.total_months_logged, 0)
+        self.assertEqual(log_ripper.total_seconds_disconnected, 0)
+        self.assertEqual(log_ripper.average_downtime, 0)
+        self.assertEqual(log_ripper.average_uninterrupted_uptime, 0)
+        self.assertEqual(log_ripper.packet_loss, 0)
 
 
-class TestLogRipperHappy(unittest.TestCase):
+class TestLogRipperClassFieldPopulate(unittest.TestCase):
     @classmethod
     def setUp(cls):
         cls.log_ripper = LogRipper()
@@ -56,30 +57,75 @@ class TestLogRipperHappy(unittest.TestCase):
 
     def test_set_began_monitoring(self):
         self.log_ripper.populate_lines_array()
-        self.log_ripper.populate_words_array()
         self.log_ripper.set_began_monitoring()
         self.assertNotEqual(self.log_ripper.began_monitoring, '')
         # 2019-12-23 08:59:51
         self.assertEqual(self.log_ripper.began_monitoring.year, 2019)
-        self.assertEqual(self.log_ripper.began_monitoring.month, 12)
-        self.assertEqual(self.log_ripper.began_monitoring.day, 23)
-        self.assertEqual(self.log_ripper.began_monitoring.hour, 8)
-        self.assertEqual(self.log_ripper.began_monitoring.minute, 59)
-        self.assertEqual(self.log_ripper.began_monitoring.second, 51)
+        self.assertEqual(self.log_ripper.began_monitoring.month, 10)
+        self.assertEqual(self.log_ripper.began_monitoring.day, 1)
+        self.assertEqual(self.log_ripper.began_monitoring.hour, 1)
+        self.assertEqual(self.log_ripper.began_monitoring.minute, 0)
+        self.assertEqual(self.log_ripper.began_monitoring.second, 0)
 
     def test_set_ended_monitoring(self):
         self.log_ripper.populate_lines_array()
-        self.log_ripper.populate_words_array()
         self.log_ripper.set_ended_monitoring()
-        self.assertEqual(self.log_ripper.ended_monitoring, '')
-
-
+        self.assertNotEqual(self.log_ripper.ended_monitoring, '')
+        # 2019-12-23 10:02:01
+        self.assertEqual(self.log_ripper.ended_monitoring.year, 2019)
+        self.assertEqual(self.log_ripper.ended_monitoring.month, 12)
+        self.assertEqual(self.log_ripper.ended_monitoring.day, 23)
+        self.assertEqual(self.log_ripper.ended_monitoring.hour, 18)
+        self.assertEqual(self.log_ripper.ended_monitoring.minute, 0)
+        self.assertEqual(self.log_ripper.ended_monitoring.second, 14)
 
 class TestLogRipperTimeParsing(unittest.TestCase):
     @classmethod
     def setUp(cls):
         cls.log_ripper = LogRipper()
         sys.argv = ['', 'testpinglog.txt']
+        cls.log_ripper.set_filename()
+        cls.log_ripper.open_file_stream()
+        cls.log_ripper.populate_lines_array()
+        cls.log_ripper.set_began_monitoring()
+        cls.log_ripper.set_ended_monitoring()
+        cls.log_ripper.calculate_totals_logged()
+
+    @classmethod
+    def tearDown(cls):
+        cls.log_ripper.file_stream.close()
+
+    def test_calculate_totals_monitored(self):
+        self.assertNotEqual(self.log_ripper.total_seconds_logged, 0)
+        self.assertNotEqual(self.log_ripper.total_minutes_logged, 0)
+        self.assertNotEqual(self.log_ripper.total_hours_logged, 0)
+        self.assertNotEqual(self.log_ripper.total_days_logged, 0)
+        self.assertNotEqual(self.log_ripper.total_weeks_logged, 0)
+        self.assertNotEqual(self.log_ripper.total_months_logged, 0)
+
+    def test_flip_connected_boolean(self):
+        self.log_ripper.set_connected(False)
+        self.assertFalse(self.log_ripper.connected)
+        self.log_ripper.set_connected(True)
+        self.assertTrue(self.log_ripper.connected)
+
+    def test_flip_not_connected_boolean(self):
+        self.log_ripper.set_not_connected(True)
+        self.assertTrue(self.log_ripper.not_connected)
+        self.log_ripper.set_not_connected(False)
+        self.assertFalse(self.log_ripper.not_connected)
+
+    def test_check_line_for_connected(self):
+        succeeded_connecting = '[1577162654.096654] 64 bytes from 216.239.38.117 (216.239.38.117): icmp_seq=48997 ttl=51 time=18.0 ms'
+        success = self.log_ripper.check_line_for_connected(succeeded_connecting)
+        self.assertTrue(self.log_ripper.connected)
+        self.assertTrue(success)
+        failed_to_connect = '[1577145503.845204] From ubuntu-box (192.168.1.105) icmp_seq=31884 Destination Host Unreachable'
+        failure = self.log_ripper.check_line_for_connected(failed_to_connect)
+        self.assertFalse(self.log_ripper.connected)
+        self.assertFalse(failure)
+
+
 
 
 class TestLogRipperSad(unittest.TestCase):
